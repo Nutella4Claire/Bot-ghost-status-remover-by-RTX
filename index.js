14b35402b7d6c4210421303d4aadc4c7f86bc011
Nutella4Claire <160255313+Nutella4Claire@users.noreply.github.com>
2024-02-16 10:29:09 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Playing Resident Evil 2"];
 
 
 let currentIndex = 0;
